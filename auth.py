40014c0699ff703ae85a6c7bffddcc65bdb275f0
EnochFrimps033 <enochfrimpong033@gmail.com>
2026-06-02 23:15:31 -0400

    Add authentication utilities

---
+++ auth.py
@@ -0,0 +1,31 @@
+import os
+from datetime import datetime, timedelta, timezone
+
+from dotenv import load_dotenv
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+laod_dotenv()
+
+SECRET_KEY = os.getenv
+ALGORITM = "HS256"#Use the "HS256 sigining algo for JWT tokens
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
+
+def hashed_password(passsword: str):
+    return pwd.context.hash(password)
+
+def verify_passwordp(plain_password:str, hashed_password:str):
+    return pwd_context.verify(plain_password,hashed_password)
+
+def create_access_token(data: dict):
+    copy_of_the_data = data.copy()
+
+    expire = datetime.now(timezone.utc) + time(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    copy_of_the_data.update({"exp":expire})
+
+    access_token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITM)
+
+    return access_token
