e32d391d81b995e045d253defef8fe5e2fd4d156
EnochFrimps033 <enochfrimpong033@gmail.com>
2026-06-04 00:21:35 -0400

    add register router

--- routes/users.py
+++ routes/users.py
@@ -1,10 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from database import get_db
+from schemas.user_schema import UserCreate, UserLogin, UserResponse, TokenResponse
+from viewmodels.auth_viewmodel import AuthViewModel
 
 router = APIRouter(
     prefix="/auth",
     tags=["Auth"]
 )
 
-@router.get("/test")
-def test_auth_route():
-    return {"message": "Auth route is working"}
+@router.post("/register, response_model=UserResponse")
+def register(user_data: UserCreate, db: Session = Depends(get_db)):
+    auth_vm=AuthViewModel(db)
+    return auth_vm.register_user(user_data)
