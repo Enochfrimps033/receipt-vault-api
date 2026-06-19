f7184f1d5b03114a56ef0072d059d23a1e1490f3
EnochFrimps033 <enochfrimpong033@gmail.com>
2026-06-19 19:03:31 -0400

    add openAI/w response

--- services/openai_service.py
+++ services/openai_service.py
@@ -1,21 +1,40 @@
+import os
 import base64
+import json
+
+from dotenv import load_dotenv
 from openai import OpenAI
 
 
 load_dotenv(dotenv_path=".env")
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+response = client.responses.create(
+    model="gpt-5.5"
+    input=[
+        {
+            "role":"user",
+            "content": [
+                {
+                    "type":"input_text",
+                    "text": """
 
 
-def extract_receipt_data(image_bytes):
-    base64_image = base64.b64encode(image_bytes).decode("utf-8")
-    
-    return {
-        "store_name": "Test Store",
-        "amount": 25.79,
-        "date": "2026-06-06",
-        "return_window": "30 days",
-        "warranty_until": None,
-        "warranty_info": "Test warranty info",
-        "image_base64_preview": base64_image[:50]
-    }
+                    Extract the receipt information from this image.
+
+Return ONLY valid JSON with these keys:
+store_name, amount, date, return_window, warranty_until, warranty_info.
+
+If a field is missing, use null.
+"""
+                },
+                {
+                    "type": "input_image",
+                    "image_url": f"data:{mime_type};base64,{base64_image}",
+                }
+            ]
+        }
+    ]
+)
+
+return json,loads(response.output_text)
