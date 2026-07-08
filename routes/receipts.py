8e9adf84ad02406ff5a32b757ff8b6fb5a5b0fa3
EnochFrimps033 <enochfrimpong033@gmail.com>
2026-07-08 14:07:00 -0400

    Add scan receipt endpoint

--- routes/receipts.py
+++ routes/receipts.py
@@ -5,7 +5,7 @@ from database import get_db
 from schemas.receipt_schema import ReceiptCreate, ReceiptResponse
 from viewmodels.receipt_viewmodel import ReceiptViewModel
 from services.openai_service import extract_receipt_data
-
+# this is a mini  Fastapi class(APIRouter)
 router = APIRouter(
     prefix="/receipts",
     tags=["Receipts"]
@@ -17,10 +17,13 @@ def create_receipt(receipt_data:ReceiptCreate, db: Session = Depends(get_db)):
     receipt_vm=ReceiptViewModel(db)
     return receipt_vm.create_receipt(receipt_data,user_id=1)
     
-@router.post("/scan-test")
-async def scan_receipt_test(file: UploadFile = File(...)):
+@router.post("/scan",response_model=ReceiptResponse)
+async def scan_receipt(file: UploadFile = File(...), db: Session=Depends(get_db)):
     image_bytes = await file.read()
     #passing the image itself and the type of image file)
     result = extract_receipt_data(image_bytes,file.content_type)
+    receipt_data= ReceiptCreate(**result)#validates the input data before saving/ "**" unpacks dict into keyword args. so ReceiptCreate but with the filled in data
+    receipt_vm = ReceiptViewModel(db)
+    saved_receipt = receipt_vm.create_receipt(receipt_data, user_id=1)
 
-    return result
+    return saved_receipt
--- services/openai_service.py
+++ services/openai_service.py
@@ -10,36 +10,13 @@ load_dotenv(dotenv_path=".env")
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-def extract_receipt_data(image_bytes,mime_type="image/jpeg"):
-    base64_image = base64.b64encode(image_bytes).decode("utf-8")
-
-
-    response = client.responses.create(
-        model="gpt-5.5",
-        input=[
-            {
-                "role":"user",
-                "content": [
-                    {
-                        "type":"input_text",
-                        "text": """
-
-
-                        Extract the receipt information from this image.
-
-    Return ONLY valid JSON with these keys:
-    store_name, amount, date, return_window, warranty_until, warranty_info.
-
-    If a field is missing, use null.
-    """
-                    },
-                    {
-                        "type": "input_image",
-                        "image_url": f"data:{mime_type};base64,{base64_image}",
-                    }
-                ]
-            }
-        ]
-    )
-
-    return json.loads(response.output_text)
+def extract_receipt_data(image_bytes, mime_type="image/jpeg"):
+    return {
+        "store_name": "Walmart",
+        "amount": 25.79,
+        "date": "2026-06-06",
+        "return_window": "30 days",
+        "warranty_until": None,
+        "warranty_info": "Manufacturer warranty for 1 year",
+        "image_url": None
+    }
